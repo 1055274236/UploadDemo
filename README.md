d52b2b7a480d36faf6b73a5bf591fa637bc87b25
Ming <1055274236@qq.com>
2022-09-10 21:35:30 +0800

    修改跨域

---
+++ README.md
@@ -0,0 +1,15 @@
+<!--
+ * @Description: 
+ * @Autor: Ming
+ * @LastEditors: Ming
+ * @LastEditTime: 2022-09-10 21:31:37
+-->
+
+## 启动参数
+
+```
+  pnpm i
+  pnpm start
+```
+
+使用 koa2 + Sequelize 创建，做文件上传与大文件下载Demo，[数据库参数修改](./src/sequelize/sequelize.ts)
--- server.ts
+++ server.ts
@@ -2,18 +2,18 @@
  * @Description:
  * @Autor: Ming
  * @LastEditors: Ming
- * @LastEditTime: 2022-09-10 21:21:47
+ * @LastEditTime: 2022-09-10 21:35:07
  */
 import koa from 'koa';
+import cors from 'koa2-cors';
 
 import Router from './src/router';
 import { throwError, koa_body } from './src/utils';
 
-const port = 50004;
+const port = 50005;
 const app = new koa();
 
-app.use(throwError());
-app.use(koa_body());
+app.use(throwError()).use(koa_body()).use(cors());
 
 app.use(Router());
 
